03d87e1a020796b321cfda88c821944eac1dcd14
TSnake41 <TSnake@Outlook.fr>
2023-07-12 10:42:27 +0200

    Fix various OpenMetrics text format issues.

--- xcp-metrics/src/publishers/openmetrics/text.rs
+++ xcp-metrics/src/publishers/openmetrics/text.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 use std::{
+    borrow::Cow,
     fmt::Write,
     time::{SystemTime, UNIX_EPOCH},
 };
@@ -21,13 +22,26 @@ fn metric_type_to_str(metric_type: MetricType) -> &'static str {
     }
 }
 
-fn escaped_string(s: &str) -> String {
+fn escape_string(s: &str) -> String {
     s.escape_default().collect()
+}
+
+fn format_name(s: &str, allow_colon: bool, keep_underscores: bool) -> String {
+    s.char_indices()
+        .filter_map(|(pos, c)| match c {
+            c @ 'A'..='Z' => Some(c),
+            c @ 'a'..='z' => Some(c),
+            c @ '0'..='9' if pos != 0 => Some(c),
+            ':' if allow_colon => Some(':'),
+            '_' if keep_underscores => Some('_'),
+            _ => None,
+        })
+        .collect()
 }
 
 pub fn write_metrics_set_text<W: Write>(writer: &mut W, metrics: &MetricSet) -> Result<()> {
     for (name, family) in &metrics.families {
-        let name = name.replace('-', "_");
+        let name = format_name(name, true, true);
 
         write_family(writer, &name, &family)?;
     }
@@ -38,18 +52,27 @@ pub fn write_metrics_set_text<W: Write>(writer: &mut W, metrics: &MetricSet) -> 
 }
 
 fn write_family<W: Write>(writer: &mut W, name: &str, family: &MetricFamily) -> Result<()> {
+    // Remove all non-ascii characters from unit.
+    let unit_escaped = format_name(&family.unit, true, false);
+
+    // Add the unit suffix (if relevant)
+    let name = if !unit_escaped.is_empty() {
+        Cow::Owned(format!("{name}_{unit_escaped}"))
+    } else {
+        Cow::Borrowed(name)
+    };
+
     writeln!(
         writer,
         "# TYPE {name} {}",
         metric_type_to_str(family.metric_type)
     )?;
 
-    //let unit = family.unit.replace(&['(', ')', '\n'], "").to_lowercase();
-    //writeln!(writer, "# UNIT {name} {}", unit)?;
+    if !name.is_empty() {
+        writeln!(writer, "# UNIT {name} {}", unit_escaped)?;
+    }
 
-    writeln!(writer, "# HELP {name} {}", escaped_string(&family.help))?;
-
-    //let new_name = format!("{name}_{unit}");
+    writeln!(writer, "# HELP {name} {}", escape_string(&family.help))?;
 
     for (_, metric) in &family.metrics {
         write_metric(writer, &name, metric)?;
@@ -75,18 +98,16 @@ fn format_timestamp(system_time: &SystemTime) -> String {
 }
 
 fn format_labels(labels: &[Label]) -> String {
-    let formatted_labels: String = labels
+    labels
         .iter()
         .map(|label| {
             format!(
                 "{}=\"{}\"",
-                label.0,
+                format_name(&label.0, false, true),
                 label.1.escape_default().collect::<String>()
             )
         })
-        .collect();
-
-    format!("{{{}}}", formatted_labels)
+        .collect()
 }
 
 fn write_metric<W: Write>(writer: &mut W, name: &str, metric: &Metric) -> Result<()> {
@@ -95,7 +116,7 @@ fn write_metric<W: Write>(writer: &mut W, name: &str, metric: &Metric) -> Result
             MetricValue::Unknown(value) | MetricValue::Gauge(value) => {
                 writeln!(
                     writer,
-                    "{name}{} {} {}",
+                    "{name}{{{}}} {} {}",
                     format_labels(&metric.labels),
                     format_number_value(value),
                     format_timestamp(&metric_point.timestamp)
