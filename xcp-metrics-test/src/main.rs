12f15524e032062959b8f0d11d61a3fa5c28752a
TSnake41 <TSnake@Outlook.fr>
2023-07-19 18:19:51 +0200

    xcp-metrics-test: Avoid sending null fields.

--- xcp-metrics-test/src/main.rs
+++ xcp-metrics-test/src/main.rs
@@ -7,25 +7,37 @@ use std::{
 use serde::Serialize;
 
 #[derive(Clone, Debug, Default, Serialize)]
+#[serde()]
 struct ForwardedRequest {
     pub m: String,
     pub uri: String,
     pub query: HashMap<String, String>,
     pub version: String,
     pub frame: bool,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub transfer_encoding: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub accept: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub content_length: Option<usize>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub auth: Option<Vec<String>>,
     pub cookie: HashMap<String, String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub task: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub subtask_of: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub content_type: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub host: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub user_agent: Option<String>,
     pub close: bool,
     pub additional_headers: HashMap<String, String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub body: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub traceparent: Option<String>,
 }
 
