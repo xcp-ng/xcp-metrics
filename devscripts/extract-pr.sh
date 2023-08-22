7d59ab780df96663f21e4187ecbabe965d56ad2e
Yann Dirson <yann.dirson@vates.fr>
2023-08-22 16:04:23 +0200

    devscripts/extract-pr.sh: expand contents for commit message
    Includes DCO in the generated commit message.
    
    Signed-off-by: Yann Dirson <yann.dirson@vates.fr>

--- devscripts/extract-pr.sh
+++ devscripts/extract-pr.sh
@@ -34,8 +34,18 @@ git add Cargo.toml
 <<EOF cat |
 WIP Introduce xcp-metrics-plugin-squeezed plugin for xcp-rrdd
 
-xcp-metrics-plugin-squeezed is meant as a drop-in replacecement for
+xcp-metrics-plugin-squeezed is meant as a drop-in replacement for
 rrdp-squeezed, as a first Rust-written brick for XAPI.
 
+Metrics are read in Xenstore using the xenstore-rs bindings of
+libxenstore, structured in a suitable way for upcoming OpenMetrics
+support, and communicated to xcp-rrdd using its v2 protocol.
+
+NOTE: this currently references external git repository for
+xenstore-rs, as we had to add support for xs_watch there and the PR is
+still pending.
+
+Signed-off-by: Teddy Astie <teddy.astie@outlook.fr>
+Reviewed-by: Yann Dirson <yann.dirson@vates.fr>
 EOF
-git commit -S -F - --author=Teddy
+git commit -F - --author=Teddy
