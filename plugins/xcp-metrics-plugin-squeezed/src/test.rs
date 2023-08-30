16624ec2eac17a3a7c48596f22bf628411d7e177
Teddy Astie <teddy.astie@outlook.fr>
2023-08-30 17:42:01 +0200

    Add a test for squeezed plugin that compares protocol v2 with original OCaml plugin.

--- plugins/xcp-metrics-plugin-squeezed/src/test.rs
+++ plugins/xcp-metrics-plugin-squeezed/src/test.rs
@@ -1,6 +1,13 @@
-use xcp_metrics_plugin_common::xenstore::{mock::MockXs, write::XsWrite, xs::XBTransaction};
+use core::slice;
 
-use crate::SqueezedInfo;
+use xcp_metrics_common::rrdd::protocol_v2::{self, values_to_raw, RrddMessageHeader, RrddMetadata};
+use xcp_metrics_plugin_common::{
+    bridge::v3_to_v2::BridgeToV2,
+    plugin::XcpPlugin,
+    xenstore::{mock::MockXs, write::XsWrite, xs::XBTransaction},
+};
+
+use crate::{SqueezedInfo, SqueezedPlugin};
 
 #[test]
 fn no_vm() {
@@ -101,4 +108,53 @@ fn multiple_vm() {
             reclaimed_max: 123456 + 111111
         }
     );
+}
+
+#[test]
+fn test_export() {
+    let xs = MockXs::default();
+    xs.write(XBTransaction::Null, "/local/domain", "").unwrap();
+
+    let mut plugin = SqueezedPlugin { xs };
+
+    let mut bridge = BridgeToV2::new();
+    bridge.update(plugin.generate_metrics().into());
+
+    let metadata = bridge.get_metadata();
+    let data = bridge.get_data();
+
+    // Generate a rrdd header
+    let (header, _) = RrddMessageHeader::generate(&values_to_raw(&data), metadata.clone());
+
+    let reference_payload = &mut include_bytes!("../tests/xcp-rrdd-squeezed").as_slice();
+    let reference_header = protocol_v2::RrddMessageHeader::parse_from(reference_payload).unwrap();
+
+    // Load payload
+    let reference_metadata: RrddMetadata = serde_json::from_reader(reference_payload).unwrap();
+
+    // Check if metadata matches
+    assert_eq!(metadata, &reference_metadata);
+
+    // Check values
+
+    // Order may not be the same between xcp-rrdd-squeezed and this plugin (due to some hashmap randomness).
+    // Compare value per value.
+    reference_metadata
+        .datasources
+        .iter()
+        .zip(reference_header.values.iter())
+        .for_each(|((reference_name, _), &reference_raw_value)| {
+            // Get matching raw value in generated data.
+            let value = metadata
+                .datasources
+                .iter()
+                .zip(header.values.iter())
+                // Get matching value
+                .find(|((name, _), _)| reference_name.as_ref() == name.as_ref())
+                // Convert DataSourceValue into raw bytes.
+                .map(|(_, value)| values_to_raw(slice::from_ref(value))[0])
+                .expect(&format!("Missing name {reference_name}"));
+
+            assert_eq!(value, reference_raw_value);
+        });
 }
