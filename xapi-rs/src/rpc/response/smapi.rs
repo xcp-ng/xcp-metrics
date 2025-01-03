7f2cfdc1454b7bfff302d54f82a2f90e64621f7d
Teddy Astie <teddy.astie@vates.tech>
2025-01-03 10:23:16 +0100

    Formatting changes

--- xapi-rs/src/rpc/response/smapi.rs
+++ xapi-rs/src/rpc/response/smapi.rs
@@ -6,15 +6,14 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, TryToValue, TryFromValue, Serialize, Deserialize, PartialEq)]
 pub struct QueryResult {
-  pub plugin: String,
-  pub name: String,
-  pub description: String,
-  pub vendor: String,
-  pub copyright: String,
-  pub version: String,
-  pub required_api_version: String,
-  pub features: Vec<String>,
-  pub configuration: HashMap<String, String>,
-  pub required_cluster_stack: Vec<String>
+    pub plugin: String,
+    pub name: String,
+    pub description: String,
+    pub vendor: String,
+    pub copyright: String,
+    pub version: String,
+    pub required_api_version: String,
+    pub features: Vec<String>,
+    pub configuration: HashMap<String, String>,
+    pub required_cluster_stack: Vec<String>,
 }
-
