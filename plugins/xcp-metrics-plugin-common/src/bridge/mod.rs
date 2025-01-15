dff3385f058116344758c804232fc8170f10e0f2
Teddy Astie <teddy.astie@vates.tech>
2025-01-15 11:51:22 +0100

    Remove legacy plugin code from source tree.
    They got deprecated with code reorganization and probably doesn't compile anymore.
    
    Signed-off-by: Teddy Astie <teddy.astie@vates.tech>

--- plugins/xcp-metrics-plugin-common/src/bridge/mod.rs
+++
@@ -1,3 +0,0 @@
-//! Bridges for conversion beteen protocol v2 and protocol v3.
-
-pub mod v3_to_v2;
--- plugins/xcp-metrics-plugin-common/src/lib.rs
+++
@@ -1,6 +0,0 @@
-pub mod bridge;
-pub mod plugin;
-pub mod protocol_v2;
-pub mod protocol_v3;
-
-//pub mod xenstore;
